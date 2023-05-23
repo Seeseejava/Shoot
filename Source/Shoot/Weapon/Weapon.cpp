86ed1642be2659ced90b9b9d4cf16b58cf938860
Seeseejava <49669869+Seeseejava@users.noreply.github.com>
2023-05-23 18:45:30 +0800

    Pickup Widget
    now the client can't show the pickup widget

--- Source/Shoot/Weapon/Weapon.cpp
+++ Source/Shoot/Weapon/Weapon.cpp
@@ -3,7 +3,8 @@
 
 #include "Weapon.h"
 #include "Components/SphereComponent.h"
-
+#include "Components/WidgetComponent.h"
+#include "Shoot/Character/ShootCharacter.h"
 
 AWeapon::AWeapon()
 {
@@ -11,7 +12,6 @@ AWeapon::AWeapon()
 	bReplicates = true;
 
 	WeaponMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("WeaponMesh"));
-	WeaponMesh->SetupAttachment(RootComponent);
 	SetRootComponent(WeaponMesh);
 
 	WeaponMesh->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Block);
@@ -22,21 +22,44 @@ AWeapon::AWeapon()
 	AreaSphere->SetupAttachment(RootComponent);
 	AreaSphere->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
 	AreaSphere->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	PickupWidget = CreateDefaultSubobject<UWidgetComponent>(TEXT("PickupWidget"));
+	PickupWidget->SetupAttachment(RootComponent);
 }
 
 
 void AWeapon::BeginPlay()
 {
 	Super::BeginPlay();
+
+
 	
 	// if (GetLocalRole() == ENetRole::ROLE_Authority)
-	if(HasAuthority())
+	if (HasAuthority())
 	{
 		AreaSphere->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
 		AreaSphere->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+		AreaSphere->OnComponentBeginOverlap.AddDynamic(this, &AWeapon::OnSphereOverlap);
+	}
+
+
+	if (PickupWidget)
+	{
+		PickupWidget->SetVisibility(false);
 	}
 }
 
+
+void AWeapon::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+
+
+	AShootCharacter* ShootCharacter = Cast<AShootCharacter>(OtherActor);
+	if (ShootCharacter && PickupWidget)
+	{
+		PickupWidget->SetVisibility(true);
+	}
+}
 
 void AWeapon::Tick(float DeltaTime)
 {
