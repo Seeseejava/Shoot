ed8bb2879c073f9f46a4f351b28e771d033009d1
Seeseejava <49669869+Seeseejava@users.noreply.github.com>
2023-06-12 18:31:06 +0800

    Hit Scan Weapon

--- Source/Shoot/ShootComponents/CombatComponent.h
+++ Source/Shoot/ShootComponents/CombatComponent.h
@@ -140,7 +140,8 @@ private:
 
 	UPROPERTY(EditAnywhere)
 	int32 StartingRocketAmmo = 0;
-
+	UPROPERTY(EditAnywhere)
+	int32 StartingPistolAmmo = 0;
 	void InitializeCarriedAmmo();
 
 	UPROPERTY(ReplicatedUsing = OnRep_CombatState)
